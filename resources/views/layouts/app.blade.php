a9dcb78aabcc3d69441dea7f48805f72a72f310e
Syarif Aminul Khoiri <aminulkhoiri@gmail.com>
2026-08-15 22:01:55 +0700

    fix: add missing scripts in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,4 +36,17 @@
       </div>
       
     </div>
->
+  </div>
+  <div class="dark-transparent sidebartoggler"></div>
+  <!-- Import Js Files -->
+  <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
+  <script src="{{ asset('assets/js/theme/app.init.js') }}"></script>
+  <script src="{{ asset('assets/js/theme/theme.js') }}"></script>
+  <script src="{{ asset('assets/js/theme/app.min.js') }}"></script>
+  <script src="{{ asset('assets/js/theme/sidebarmenu.js') }}"></script>
+
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+</html>

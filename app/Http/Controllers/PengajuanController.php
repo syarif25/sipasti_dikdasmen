ca2c1c199537acf5b770f65857a33443dc21d82f
Syarif Aminul Khoiri <aminulkhoiri@gmail.com>
2026-08-16 22:49:17 +0700

    feat: restore Tujuan dropdown in create view and remove hardcoded backend override

--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -77,11 +77,7 @@ class PengajuanController extends Controller
         $pengajuan->nomor_surat = $request->nomor_surat;
         $pengajuan->jenis_surat = $request->jenis_surat;
         $pengajuan->perihal = $request->perihal;
-        if (auth()->user()->level == 1) {
-            $pengajuan->tujuan = 'Admin Dikdasmen';
-        } else {
-            $pengajuan->tujuan = collect(Jabatan::where('id_jabatan', $request->tujuan)->first())->get('nama_jabatan', $request->tujuan);
-        }
+        $pengajuan->tujuan = collect(Jabatan::where('id_jabatan', $request->tujuan)->first())->get('nama_jabatan', $request->tujuan);
         $pengajuan->ket = $request->ket;
         $pengajuan->tgl_upload = now();
         $pengajuan->id_tahun = $idTahun;
